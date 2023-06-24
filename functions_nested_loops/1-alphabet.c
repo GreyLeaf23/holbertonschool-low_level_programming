37cb6871088f22ee80fd60f2b3fc48ffd8a370f9
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-24 18:02:07 +0000

    Prints the alphabet in lowercase using the 'void print_alphabet(void);' prototype.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+
+int main(void)
+
+	char alphabet;
+{
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+{
+_putchar(alphabet);
+}
+
+_putchar("\n");
+return (0);
+
+}
