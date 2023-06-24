85216014318b34bca3f7a477b84f96ce313fea81
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-24 18:55:30 +0000

    Multiple Files (Clearing Git Status)

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase.
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0
  */
 
 void print_alphabet(void);
+
 {
 {
 	char alphabet;
