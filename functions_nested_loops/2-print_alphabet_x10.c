17111acfcf8395faaa7f4112336dcfe4222447b6
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-25 01:01:57 +0000

    Program that prints the alphabet 10 times.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 {
 
 	char alphabet;
-	int i = 10;
+
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
 		_putchar(alphabet);
-
-	while (alphabet * i)
+	}
 
 _putchar('\n');
 
