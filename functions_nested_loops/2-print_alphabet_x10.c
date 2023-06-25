28cc48f6503a315328b08c3857193b18c7c786a8
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-25 03:36:05 +0000

    Function that displays the alphabet, 10 times.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,18 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int i;
 
+	for (i = 0; i < 10; i++)
+	
+	{
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
 	{
 
-	for (i = 10; i < 10; i++)
-	{
 		_putchar(alphabet);
 	}
 
 _putchar('\n');
 
+	}
 }
