4503a21195f394ec16df606fc755b162c60e0545
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-28 16:28:14 +0000

    Function that check ofr lowercase character.

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,11 +3,25 @@
 /**
  * int _islower(int c) - Function that checks for lowercase character.
  *
+ * main - Entry Point
  * Return: Always 0
  */
 
-
 int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
+
+int main(void)
 {
 
 	int c;
