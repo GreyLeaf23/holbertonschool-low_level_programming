b29b482b4ded605e1de641dc5629845557c22174
GreyLeaf23 <christian.torres2894@gmail.com>
2023-06-29 22:21:44 +0000

    Hotfix.

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,9 +6,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-<<<<<<< HEAD
 int print_sign(int n);
-=======
->>>>>>> 48f9201eea7b977fc3279a1dff208e0f6d560e3a
 
 #endif
