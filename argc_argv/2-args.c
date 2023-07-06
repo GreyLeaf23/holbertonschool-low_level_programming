c67aa183e80d622590b5df5de51df549ceae21cd
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-06 21:56:19 +0000

    Program that prints all arguments that it receives.

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -13,11 +13,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("%d\n", argc);
-
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
