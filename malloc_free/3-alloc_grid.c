f80299e90fcdf411b0e0d7dc1e12400008726ac6
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-14 17:57:35 +0000

    Function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,44 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
+ * @width: width input, first dim.
+ * @height: height input, second dim.
+ *
  * Return: pointer to 2 dim. array
  */
-int **alloc_grid(int width, int height)
+int **alloc_grid(int width, int height) /*Double pointer indicated by prototype.*/
 {
-	int **mee;
-	int x, y;
+	int **grid;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0) /*Two 'if statements' using pipeline.*/
 		return (NULL);
 
-	mee = malloc(sizeof(int *) * height);
+	/*Allocate space for height.*/
+	grid = (int **) malloc(height * sizeof(int *));
 
-	if (mee == NULL)
+	if (grid == NULL)
 		return (NULL);
 
-	for (x = 0; x < height; x++)
+	/*Allocate space for width inside of a loop.*/
+	for (i = 0; i < height; i++)
 	{
-		mee[x] = malloc(sizeof(int) * width);
-
-		if (mee[x] == NULL)
+			grid[i] = (int *) malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
-			for (; x >= 0; x--)
-				free(mee[x]);
-
-			free(mee);
+			/*Free previously allocated memory*/
+			for (; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
 			return (NULL);
 		}
 	}
-
-	for (x = 0; x < height; x++)
+	/*Set all elements to 0.*/
+	for (i = 0; i < height; i++)
 	{
-		for (y = 0; y < width; y++)
-			mee[x][y] = 0;
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
-
-	return (mee);
+	return (grid);
 }
