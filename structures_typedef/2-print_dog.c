6a158d75e86c0c3c308e20116f7132f45d5900d5
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-17 18:12:04 +0000

    Function taht prints the values of the struct dog's variables.

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - output the values of the structure's variables.
+ * @d: points to the variables in the struct.
+ *
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %d\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+
+	else
+		printf("Owner: %s\n", d->owner);
+}
