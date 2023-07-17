e6f210ecc1638f8dd186aafef805bca315f87ae1
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-17 18:40:20 +0000

    New variable to 'struct dog'.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,17 +7,17 @@
  * @name: dog's name.
  * @age: dog's age.
  * @owner: dog's master.
- *
+ * @dog_t: new type.
  */
 
 struct dog
 {
- char *name;
- float age;
- char *owner;
- int dog_t;
+char *name;
+float age;
+char *owner;
+int dog_t;
 };
-/* */
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
