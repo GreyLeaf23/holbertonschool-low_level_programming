ff4298b6a6de8a1ab7b30c4077ff164eac82b13f
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-17 14:37:06 +0000

    Create header file with struct name 'dog'.

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - structure holding variables.
+ *
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's master.
+ *
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
