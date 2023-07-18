f84f300da0491b58d00af589f44779d3174fd8d1
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-18 14:39:08 +0000

    Function that stores the variables 'name' and 'owner' to a new structure.

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * new_dog - represents the structure 'dog_t'.
+ * @name: string to store.
+ * @owner: string to store.
+ *
+ * Return: new_dog struct.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog; /*Sub-name for struct*/
+
+	/*Make space for struct*/
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return;
+
+	/*Make space for the lenght of name.*/
+	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return;
+	}
+
+	/*Make space for the length of owner.*/
+	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name); /*Free space of previously-made functions.*/
+		free(new_dog);
+		return;
+	}
+
+	/*Make duplication of 'name' and 'owner'*/
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
+	new_dog->age = age;
+
+	return (new_dog);
+}
