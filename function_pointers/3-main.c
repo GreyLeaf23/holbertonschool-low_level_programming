4563c330e7afe30bb31a2b12c3cb981cb0021997
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-21 01:53:33 +0000

    Program that performs simple operations.

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,17 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	int n1, n2;
-	int  result = 0;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
 
 	if (get_op_func(argv[2]) == NULL)
 	{
@@ -30,13 +24,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (argv[2] == '/' || argv[2] == '%')
+	if (argv[3] == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", argv[2],(n1, n2));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 
 return (0);
 }
