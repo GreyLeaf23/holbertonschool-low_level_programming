6140acac2aa4a9a658809600f7293ce439b12afe
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-21 20:35:29 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,36 +1,35 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - receive input from user.
- * @argc: amount of arguments.
- * @argv: memory held for arguments.
- *
- * Return: 0.
+ * main - check the code for Holberton School students.
+ * @argc: arg count
+ * @argv: pointers to arguments
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (get_op_func(argv[2]) == NULL)
+	if (strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if (argv[3] == 0)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-
-return (0);
+	result = (*get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
+
