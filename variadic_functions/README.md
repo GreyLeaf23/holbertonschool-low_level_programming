2ce7d3e052a0aa0c2e55e0c5e3186df42c9024ba
GreyLeaf23 <christian.torres2894@gmail.com>
2023-07-22 01:10:59 +0000

    Function that returns the sum of all its parameters.

---
+++ variadic_functions/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+C - Variadic Functions Learning.
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+int _putchat(char);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
