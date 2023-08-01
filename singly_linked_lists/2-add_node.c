51696a8c8a9afa3e0c8f690fb6856c47bfe69d67
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-01 17:37:19 +0000

    Function that adds a new node at the beginning of a linked list.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,29 +10,28 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-size_t index = 0;
-int i = 0;
+	list_t *new_node;
 
-list_t *new_node = malloc(sizeof(list_t));
-new_node->str = strdup(str);
-
-	if (new_node == NULL || str == NULL)
+	if (str == NULL)
 	{
-	return (NULL);
-	}
-		else if (new_node->str[i] == '\0')
-		{
 		return (NULL);
-		}
-			else
-			{
-				while (head != NULL)
-				{
-				new_node->next = *head;
-				*head = new_node;
-				index++;
-				}
-			}
+	}
 
-return (new_node);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
+
