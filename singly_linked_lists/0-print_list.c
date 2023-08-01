84b8e1e56df13afa77acafa70aa137824cc699e5
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-01 01:29:35 +0000

    Function that prints all the elements of linked list.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,18 +2,18 @@
 
 /**
  * print_list - printing elements of 'list_t' list.
- * @h: pointer to list.
+ * @h: pointer to linked list's structure.
  *
  * Return: the number of nodes.
  */
 
 size_t print_list(const list_t *h)
 {
-size_t index = 0;
+size_t index = 0; /*Measuring point for the list.*/
 
-	while (h != NULL)
+	while (h != NULL) /*Conditional.*/
 	{
-		if (h->str == NULL)
+		if (h->str == NULL) /*Conditional.*/
 		{
 		printf("[0] (nil)\n");
 		}
@@ -26,4 +26,4 @@ size_t index = 0;
 	}
 
 return (index);
-}
+
