a876aed30a81ba0b1eb998382451ebb5bdb36648
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-03 02:05:02 +0000

    Function that adds new node at the end of linked list.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -22,8 +22,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = strlen(str);
 
 	if (new_node->str == NULL)
+	{
+	free(new_node);
 	return (NULL);
-
+	}
 	new_node->next = NULL;
 
 	if (*head == NULL)
