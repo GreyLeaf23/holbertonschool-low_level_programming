6d55f863f190e10c620ee6a9fb73926f74a5bcb3
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-04 19:55:55 +0000

    Function that returns a node from linked list.

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,12 +13,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *get_node = head;
 	unsigned int num = 0;
 
-	if (num == index)
-		return (get_node);
-
-	else
 	while (get_node != NULL)
 	{
+		if (num == index)
+			return (get_node);
 		get_node = get_node->next;
 		num++;
 	}
