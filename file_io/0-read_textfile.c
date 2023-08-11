fa20747837e313833ce42bce949fefd93ea8770f
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-11 00:57:08 +0000

    Function that reads a text file and prints the amount of letters it holds.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a file.
+ * @filename: pointer towards file being read.
+ * @letters: amount of letters in a file.
+ *
+ * Return: letters that were counted.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd, count, prt;
+char *buffer;
+
+if (filename == NULL) /*Error Proof.*/
+{
+	return (0);
+}
+
+fd = open(filename, O_RDONLY); /*Opening a file to be read only.*/
+
+buffer = malloc(sizeof(char) * letters); /*Space for amount of letters.*/
+
+if (buffer == NULL) /*Error Proof.*/
+{
+	return (0);
+}
+
+/*Reads letters in the file that are being counted.*/
+count = read(fd,  buffer, letters);
+
+/*Writes amount of 'char's being counted.*/
+prt = write(STDOUT_FILENO, buffer, count);
+
+if (prt == '\0') /*Error Proof.*/
+{
+	return (0);
+}
+
+free(buffer); /*Free space to contain letters.*/
+close(fd); /*Closes file being managed.*/
+
+return (count);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 /*Function Prototypes.*/
 
