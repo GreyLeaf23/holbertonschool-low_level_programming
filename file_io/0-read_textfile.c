34c7d9903d9ec94dae381886c89251f979449536
GreyLeaf23 <christian.torres2894@gmail.com>
2023-08-11 01:17:28 +0000

    Function that reads a text file and prints the amount of letters it holds.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,18 +14,17 @@ int fd, count, prt;
 char *buffer;
 
 if (filename == NULL) /*Error Proof.*/
-{
 	return (0);
-}
+
 
 fd = open(filename, O_RDONLY); /*Opening a file to be read only.*/
+if (fd == -1)
+	return (0);
 
 buffer = malloc(sizeof(char) * letters); /*Space for amount of letters.*/
 
 if (buffer == NULL) /*Error Proof.*/
-{
 	return (0);
-}
 
 /*Reads letters in the file that are being counted.*/
 count = read(fd,  buffer, letters);
@@ -34,9 +33,7 @@ count = read(fd,  buffer, letters);
 prt = write(STDOUT_FILENO, buffer, count);
 
 if (prt == '\0') /*Error Proof.*/
-{
 	return (0);
-}
 
 free(buffer); /*Free space to contain letters.*/
 close(fd); /*Closes file being managed.*/
