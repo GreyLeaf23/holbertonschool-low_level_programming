8deaf457edf6fa808a2d188b024ee166173c4407
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-22 08:26:00 -0400

    Changed print statement

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -22,7 +22,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 
     /*Print the current value being checked*/
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 		{
