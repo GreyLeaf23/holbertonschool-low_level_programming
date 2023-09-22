1dff02ed64f532b0d815ebe45dc8d39a4b2c61b6
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-22 08:30:17 -0400

    tweak file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,23 +14,17 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (array == NULL)
-	{
 		return (-1); /*Return -1 if array is NULL*/
-	}
 
 	while (i < size)
 	{
-
-    /*Print the current value being checked*/
+	 /*Print the current value being checked*/
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-		{
 			return (i); /*Return the index if value is found.*/
-		}
 
 		i++;
 	}
-
 	return (-1); /*Return -1 if value is not found in array.*/
 }
